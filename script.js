6e365b04457d7b2535410f82a0487a5337ed6ad3
Tajetti <talhettialvaro@gmail.comm>
2025-04-18 19:33:34 -0300

    Finalizando a adição de elementos na lista rápida

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const form = document.getElementById("form")
-const addItem = document.getElementById("addItem")
-const submit = document.getElementById("submit")
-const list = document.getElementsByClassName("list")
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const list = document.getElementById('lista');
+const trash = document.querySelector('.trash');
 
-addItem.addEventListener("input", (event) => {
-  const hasNumberRegex = /\d+/g;
-  addItem.value = addItem.value.replace(hasNumberRegex, "");
-}) // Verifica se o input que adiciona itens tem números. Se tiver, ele irá remover
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nomeItem = input.value
+  
+  if(nomeItem === "") {
+    alert("Digite um item!");
+    return;
+  }
 
-form.onsubmit = (event) => { // Adiciona o evento de submit ao form
-  event.preventDefault() // Previne que a página atualize
-
-  const itemParaAdicionar = addItem.value // Verifica se o input está vazio e remove os espaços em branco
-  if (itemText === "") {return} // Se o input estiver vazio, não faz nada
-}
-
+  const li = document.createElement("li");
+  li.className = "option"
+  li.innerHTML = `
+    <div class="item">
+      <input type="checkbox" class="checkbox" id="checkbox">
+      <label for="checkbox">${nomeItem}</label>
+    </div>
+    <div class="trash">
+      <img src="img/icon/Frame-3.png" alt="Lixeira" class="trash-icon"> 
+    </div>
+    `
+  lista.appendChild(li);
+  input.value = ""; 
+})
