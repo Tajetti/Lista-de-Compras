4f12f46229edc1692b37b277e2a2c6d71cd05fda
Tajetti <talhettialvaro@gmail.comm>
2025-04-19 10:17:02 -0300

    lendo a adicindando comentários para fins didáticos

--- script.js
+++ script.js
@@ -4,36 +4,36 @@ const list = document.getElementById('lista');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const nomeItem = input.value
+  const nomeItem = input.value // Ele pega o valor do input (o que o usuário digitou)
   
   if(nomeItem === "") {
     alert("Digite um item!");
     return;
-  }
+  } // Verificação de que se o input estiver vazio, ele alerta que está vazio.
 
-  const li = document.createElement("li");
-  li.className = "option"
-  li.innerHTML = `
+  const li = document.createElement("li"); // Cria um elemento li para adicionar na lista
+  li.className = "option" // Adiciona a classe option para o li
+  li.innerHTML = ` 
     <div class="item">
       <input type="checkbox" class="checkbox" id="checkbox">
-      <label for="checkbox">${nomeItem}</label>
+      <label for="checkbox">${nomeItem}</label> <!--Item adicionado pelo usuário -->
     </div>
     <div class="trash">
       <img src="img/icon/Frame-3.png" alt="Lixeira" id="trash-icon"> 
     </div>
-    `
-  lista.appendChild(li);
-  input.value = ""; 
+    ` // Adiciona o HTML dentro do li, com o input checkbox e o label com o nome do item
+  list.appendChild(li); // Adiciona o li dentro da lista
+  input.value = ""; // Retorna o valor do input para vazio 
  
-  const trashIcon = li.querySelector("#trash-icon");
-  trashIcon.addEventListener("click", () => {
-    li.remove();
+  const trashIcon = li.querySelector("#trash-icon"); // Seleciona o ícone de lixeira dentro do li
+  trashIcon.addEventListener("click", () => { 
+    li.remove(); // Remove o li
 
-    const msgError = document.querySelector(".Display-none")
-    msgError.classList.remove("Display-none")
+    const msgError = document.querySelector(".Display-none") // Seleciona a mensagem de erro em que a classe é Display-none
+    msgError.classList.remove("Display-none") // Remove a classe Display-none para mostrar a mensagem de erro
 
     setTimeout(() => {
       msgError.classList.add("Display-none")
-    }, 2000)
+    }, 2000) // Adiciona um temporizador de 2 segundos para esconder a mensagem de erro
   })
 })
